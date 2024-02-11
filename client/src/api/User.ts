146af2d87a30236cfa59ee16fbe16812ce7e0f95
Виктория Голубова <v.golubova@denexy.com>
2024-02-11 21:39:22 +0400

    Created UserSchema, functions logoutUser and fetchMe.

--- client/src/api/User.ts
+++ client/src/api/User.ts
@@ -1,4 +1,12 @@
 import {validateResponse} from "./validateResponse";
+import {z} from "zod";
+
+const UserSchema = z.object({
+    id: z.string(),
+    email: z.string(),
+    username: z.string(),
+});
+type User = z.infer<typeof UserSchema>;
 
 export function registerUser(username: string, email: string, password: string): Promise<void> {
     return fetch('/api/register', {
@@ -8,6 +16,7 @@ export function registerUser(username: string, email: string, password: string):
         },
         body: JSON.stringify({ username, email, password }),
     })
+        .then(validateResponse)
         .then(() => undefined);
 }
 
@@ -21,4 +30,19 @@ export function loginUser(email: string, password: string): Promise<void> {
     })
         .then(validateResponse)
         .then(() => undefined);
+}
+
+export function logoutUser(): Promise<void> {
+    return fetch('/api/logout', {
+        method: 'POST',
+    })
+        .then(validateResponse)
+        .then(() => undefined);
+}
+
+export function fetchMe(): Promise<User> {
+    return fetch('api/users/me')
+        .then(validateResponse)
+        .then((response) => response.json())
+        .then((data) => UserSchema.parse(data));
 }
