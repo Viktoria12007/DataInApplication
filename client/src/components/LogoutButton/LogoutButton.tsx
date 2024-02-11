07765c656365893577af02c48594eeaf5276ff2e
Виктория Голубова <v.golubova@denexy.com>
2024-02-11 22:55:37 +0400

    Added logic logoutMutation for LogoutButton.tsx

--- client/src/components/LogoutButton/LogoutButton.tsx
+++ client/src/components/LogoutButton/LogoutButton.tsx
@@ -1,10 +1,26 @@
 import { Button } from "../Button";
 import "./LogoutButton.css";
+import {logoutUser} from "../../api/User";
+import {useMutation} from "@tanstack/react-query";
+import {queryClient} from "../../api/queryClient";
 
 export const LogoutButton = () => {
+    const logoutMutation = useMutation({
+        mutationFn: () => logoutUser(),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["me"] });
+        }
+    }, queryClient);
+
   return (
     <div className="logout-button">
-      <Button kind="secondary">Выйти</Button>
+      <Button
+          kind="secondary"
+          onClick={() => logoutMutation.mutate()}
+          isLoading={logoutMutation.isPending}
+      >
+          Выйти
+      </Button>
     </div>
   );
 };
