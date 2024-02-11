78262205ff08f6774b5ef7d69a45810d957a9da6
Виктория Голубова <v.golubova@denexy.com>
2024-02-11 20:29:24 +0400

    Added logic state for RegisterForm.tsx

--- client/src/components/RegisterForm/RegisterForm.tsx
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -1,20 +1,37 @@
 import { FormField } from "../FormField";
 import { Button } from "../Button";
 import "./RegisterForm.css";
+import {FC, FormEvent, useState} from "react";
+import {useMutation} from "@tanstack/react-query";
+import {registerUser} from "../../api/User";
+import {queryClient} from "../../api/queryClient";
 
-export const RegisterForm = () => {
+export const RegisterForm: FC = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const registerMutation = useMutation({
+        mutationFn: () => registerUser(username, email, password),
+    }, queryClient);
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        registerMutation.mutate();
+    }
+
   return (
-    <form className="register-form">
+    <form className="register-form" onSubmit={handleSubmit}>
       <FormField label="Имя">
-        <input />
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
       </FormField>
       <FormField label="Email">
-        <input />
+        <input value={email} onChange={(e) => setEmail(e.target.value)} />
       </FormField>
       <FormField label="Пароль">
-        <input type="password" />
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
       </FormField>
-      <Button>Зарегистрироваться</Button>
+      <Button type="submit" isLoading={registerMutation.isPending}>Зарегистрироваться</Button>
     </form>
   );
 };
