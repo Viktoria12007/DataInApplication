54b58b12676796724a6ed19cbd2653d78d0d9b11
Виктория Голубова <v.golubova@denexy.com>
2024-02-12 15:30:50 +0400

    Created a hook FetchNotesListView.tsx to get data.

---
+++ client/src/components/NotesListView/FetchNotesListView.tsx
@@ -0,0 +1,25 @@
+import {FC} from "react";
+import {useQuery} from "@tanstack/react-query";
+import {fetchNotesList} from "../../api/Note";
+import {queryClient} from "../../api/queryClient";
+import {Loader} from "../Loader";
+import {NotesListView} from "./NotesListView";
+
+export const FetchNotesListView: FC = () => {
+    const notesListQuery = useQuery({
+       queryFn: () => fetchNotesList(),
+       queryKey: ["notes"],
+    }, queryClient);
+
+    switch (notesListQuery.status) {
+        case "pending":
+            return <Loader />;
+        case "success":
+            return <NotesListView />;
+        case "error":
+            return <div>
+                    <span>Произошла ошибка :(</span>
+                    <button onClick={() => notesListQuery.refetch()}>Повторить запрос</button>
+                   </div>
+    }
+}
