932bb1411977574608bc81ea34f7d12b759c3be6
Виктория Голубова <v.golubova@denexy.com>
2024-02-12 17:33:08 +0400

    Added logic state for NoteForm.tsx

--- client/src/components/NoteForm/NoteForm.tsx
+++ client/src/components/NoteForm/NoteForm.tsx
@@ -1,17 +1,37 @@
 import { FormField } from "../FormField";
 import { Button } from "../Button";
 import "./NoteForm.css";
+import {useState} from "react";
+import {useMutation} from "@tanstack/react-query";
+import {queryClient} from "../../api/queryClient";
+import {createNote} from "../../api/Note";
 
 export const NoteForm = () => {
+    const [title, setTitle] = useState('');
+    const [text, setText] = useState('');
+
+    const noteFormMutation = useMutation({
+        mutationFn: () => createNote(title, text),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["notes"] });
+        }
+    }, queryClient);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        noteFormMutation.mutate();
+    }
+
   return (
-    <form className="note-form">
+    <form className="note-form" onSubmit={handleSubmit}>
       <FormField label="Заголовок">
-        <input type="text" />
+        <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" />
       </FormField>
       <FormField label="Текст">
-        <textarea />
+        <textarea value={text} onChange={(e) => setText(e.target.value)} />
       </FormField>
-      <Button>Сохранить</Button>
+      { noteFormMutation.error && <span>{noteFormMutation.error.message}</span> }
+      <Button type="submit" isLoading={noteFormMutation.isPending}>Сохранить</Button>
     </form>
   );
 };
