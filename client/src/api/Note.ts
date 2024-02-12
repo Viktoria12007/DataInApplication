a61d11157776b12a3d57d9a4bcc17d7c1ecd15c8
Виктория Голубова <v.golubova@denexy.com>
2024-02-12 15:28:15 +0400

    Added functions fetchNotesList and createNote.

---
+++ client/src/api/Note.ts
@@ -0,0 +1,39 @@
+import {validateResponse} from "./validateResponse";
+import {z} from "zod";
+
+const NoteSchema = z.object({
+    id: z.string(),
+    title: z.string(),
+    text: z.string(),
+    userId: z.string(),
+    createdAt: z.number(),
+});
+type Note = z.infer<typeof NoteSchema>;
+
+const NotesListSchema = z.array(NoteSchema);
+type NotesList = z.infer<typeof NotesListSchema>;
+
+const FetchNotesList = z.object({
+    list: NotesListSchema,
+    pageCount: z.number(),
+})
+type FetchNotesList = z.infer<typeof FetchNotesList>;
+
+export function fetchNotesList(): Promise<FetchNotesList> {
+    return fetch('/api/notes')
+        .then(validateResponse)
+        .then((response) => response.json())
+        .then((data) => FetchNotesList.parse(data));
+}
+
+export function createNote(title: string, text: string): Promise<void> {
+    return fetch('/api/notes', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, text }),
+    })
+        .then(validateResponse)
+        .then(() => undefined);
+}
